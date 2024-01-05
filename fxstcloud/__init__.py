67c45d7748bad3764e3ead2f349e2a504188515c
Timo <149528391+Timoo12@users.noreply.github.com>
2024-01-05 03:20:42 +0100

    Bump version to 0.5.2

--- fxstcloud/__init__.py
+++ fxstcloud/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "fxstcloud"
 __author__ = "Chill_Fabo"
 __license__ = "MIT"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from . import blacklist, components, emb, logs, sql, times, utils
 from .bot import *
